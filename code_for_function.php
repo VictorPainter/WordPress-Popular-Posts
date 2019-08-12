c9ac93427153df236eec5fcf1369946d525cc6f6
Victor Dev <victor@DESKTOP-ETKNPC5.localdomain>
2019-08-12 18:09:26 +0300

    Popular posts counter

---
+++ code_for_function.php
@@ -0,0 +1,36 @@
+<?php 
+
+/*
+ *
+ * This code contains two functions.
+ * The first function checks and sets the hit count for the post. 
+ * The second function hooks into wp_head and calls the first function whenever a single post is loaded. 
+ * 
+ */
+
+//Place this code into function.php file
+
+function shapeSpace_popular_posts($post_id) {
+	$count_key = 'popular_posts';
+	$count = get_post_meta($post_id, $count_key, true);
+	if ($count == '') {
+		$count = 0;
+		delete_post_meta($post_id, $count_key);
+		add_post_meta($post_id, $count_key, '0');
+	} else {
+		$count++;
+		update_post_meta($post_id, $count_key, $count);
+	}
+}
+
+function shapeSpace_track_posts($post_id) {
+	if (!is_single()) return;
+	if (empty($post_id)) {
+		global $post;
+		$post_id = $post->ID;
+	}
+	shapeSpace_popular_posts($post_id);
+}
+add_action('wp_head', 'shapeSpace_track_posts');
+
+?>
